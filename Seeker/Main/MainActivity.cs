d16894628ea6bce51f3ce88b7e087f15cbec97a6
Smooth-E <smoothie@disroot.org>
2025-03-30 03:03:26 +0300

    refactor: Move RecentUserManager into a separate file at Seeker.Managers

--- Seeker/Main/MainActivity.cs
+++ Seeker/Main/MainActivity.cs
@@ -7318,46 +7318,7 @@ namespace Seeker
     }
 
     //TODOORG Managers
-    /// <summary>
-    /// Manages recent users
-    /// </summary>
-    public class RecentUserManager
-    {
-        private object recentUserLock = new object();
-        private List<string> recentUsers;
-        /// <summary>
-        /// Called at startup
-        /// </summary>
-        /// <param name="_recentUsers"></param>
-        public void SetRecentUserList(List<string> _recentUsers)
-        {
-            recentUsers = _recentUsers;
-        }
 
-        public List<string> GetRecentUserList()
-        {
-            lock (recentUserLock)
-            {
-                return recentUsers.ToList(); //a copy to avoid threading issues.
-            }
-        }
-
-        public void AddUserToTop(string user, bool andSave)
-        {
-            lock (recentUserLock)
-            {
-                if (recentUsers.Contains(user))
-                {
-                    recentUsers.Remove(user);
-                }
-                recentUsers.Insert(0, user);
-            }
-            if (andSave)
-            {
-                SeekerApplication.SaveRecentUsers();
-            }
-        }
-    }
 
     /// <summary>
     /// for the lower assembly
---
+++ Seeker/Managers/RecentUserManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Seeker.Managers;
+
+/// <summary>
+/// Manages recent users
+/// </summary>
+public class RecentUserManager
+{
+    private object recentUserLock = new object();
+    private List<string> recentUsers;
+    /// <summary>
+    /// Called at startup
+    /// </summary>
+    /// <param name="_recentUsers"></param>
+    public void SetRecentUserList(List<string> _recentUsers)
+    {
+        recentUsers = _recentUsers;
+    }
+
+    public List<string> GetRecentUserList()
+    {
+        lock (recentUserLock)
+        {
+            return recentUsers.ToList(); // a copy to avoid threading issues.
+        }
+    }
+
+    public void AddUserToTop(string user, bool andSave)
+    {
+        lock (recentUserLock)
+        {
+            if (recentUsers.Contains(user))
+            {
+                recentUsers.Remove(user);
+            }
+            recentUsers.Insert(0, user);
+        }
+        
+        if (andSave)
+        {
+            SeekerApplication.SaveRecentUsers();
+        }
+    }
+}
