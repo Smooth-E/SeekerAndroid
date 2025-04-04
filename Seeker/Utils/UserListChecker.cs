0793544e6f449fba958b5ad673b8ae93cc92a1ce
Smooth-E <smoothie@disroot.org>
2025-04-04 04:18:37 +0300

    refactor: Delete leftover UserListChecker.cs file

--- Seeker/Utils/UserListChecker.cs
+++
@@ -1,9 +0,0 @@
-﻿using SlskHelp;
-
-namespace Seeker.Utils;
-
-/// <summary>
-/// for the lower assembly // TODO: What does the summary here mean?
-/// </summary>
-// TODO: Move this and other user list utilities to a separate and more concise util class
-
