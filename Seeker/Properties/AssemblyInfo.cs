e77a2adcac4d7ae201b34dbcf5b138061ff14cd5
Smooth-E <smoothie@disroot.org>
2025-04-07 00:50:48 +0300

    refactor: Update AssemblyInfo.cs

--- Seeker/Properties/AssemblyInfo.cs
+++ Seeker/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using Android.App;
+
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
